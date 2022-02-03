a9568a086894678c0b4255c12cd4d538f525960b
Leo Speidel <leo.speidel@outlook.com>
2022-02-03 08:49:30 +0000

    Added back external data

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // timesTwo
 NumericVector timesTwo(NumericVector x);
 RcppExport SEXP _relater_timesTwo(SEXP xSEXP) {
---
+++ tests/testthat/test_basics_parse.R
@@ -0,0 +1,11 @@
+context("Testing parsing functions")
+library(relater)
+
+#test_that("parsing haps files",{
+
+#  haps <- read.haps(system.file("extdata", "example.haps.gz", package = "relater", mustWork = T))
+  #Check requirements for haps for Relate
+  #expect_equal(length("abc"), 3)
+#  print(head(haps))
+
+#})
